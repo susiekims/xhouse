c6f2b90369ec4b752c64f4373fdfff83235924d9
Susie <susie.kim@hotmail.com>
2018-10-09 17:03:01 -0400

    fixed mobile nav

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('#nav-icon3');
 const nav = document.querySelector("#nav");
+const links = document.getElementsByClassName('nav_link');
+console.log(links);
 
 button.onclick = function() {
   	button.classList.toggle('open');
@@ -9,6 +11,22 @@ button.onclick = function() {
         nav.style.display = "flex";
     }
 }
+
+for (let i = 0; i < links.length; i++) {
+    if (window.innerWidth < 620) {
+        links[i].onclick = function(){
+            console.log('click');
+            nav.style.display = "none";
+            button.classList.remove('open');
+        }
+    }
+};
+
+window.onresize = function() {
+    document.body.height = window.innerHeight;
+}
+
+window.onresize(); // called to initially set the height
 
 window.addEventListener("resize", function() {
    if (window.innerWidth > 620 ) {
