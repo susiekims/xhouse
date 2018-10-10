7ec887e59e9c4cd3c8b6490ae5ea07b954bb1abd
Susie <susie.kim@hotmail.com>
2018-10-09 23:30:37 -0400

    added hover effects

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -3,6 +3,9 @@ const nav = document.querySelector("#nav");
 const header = document.querySelector("#header");
 const links = document.getElementsByClassName('nav_link');
 const scroll = document.getElementsByClassName('scroll');
+const scroll2 = document.getElementsByClassName('scroll2');
+const scroll3 = document.getElementsByClassName('scroll3');
+const random = document.getElementsByClassName('random');
 
 button.onclick = function() {
   	button.classList.toggle('open');
@@ -36,17 +39,55 @@ window.onscroll = function () {
         if (elem.top < (window.innerHeight * .9)) {
             scroll[i].style.transform = "translateY(0px)"
             scroll[i].style.opacity = "1"
+
         } else {
             scroll[i].style.transform = "translateY(100px)"
             scroll[i].style.opacity = "0"
         }
     }
+
+    for (let i = 0; i < scroll2.length; i++) {
+        let elem = scroll2[i].getBoundingClientRect();
+        if (elem.top < (window.innerHeight * .8)) {
+            scroll2[i].style.transform = "translateY(0px)"
+            scroll2[i].style.opacity = "1"
+
+        } else {
+            scroll2[i].style.transform = "translateY(100px)"
+            scroll2[i].style.opacity = "0"
+        }
+    }
+    for (let i = 0; i < scroll3.length; i++) {
+        let elem = scroll3[i].getBoundingClientRect();
+        if (elem.top < (window.innerHeight * .7)) {
+            scroll3[i].style.transform = "translateY(0px)"
+            scroll3[i].style.opacity = "1"
+
+        } else {
+            scroll3[i].style.transform = "translateY(100px)"
+            scroll3[i].style.opacity = "0"
+        }
+    }
+   
 }
+
+
+// for (let i = 0; i < random.length; i ++) {
+//     // let num = random[i].getBoundingClientRect();
+//     let count = parseInt(random[i].innerHTML)
+//     // console.log(count);
+//     for (let i = 0; i < count; i++) {
+//         setInterval( function() {
+//             random[i].innerHTML = i;
+//             // console.log(i);
+//         }, 1000);
+//     }
+// }
 
 window.onresize(); // called to initially set the height
 
 window.addEventListener("resize", function() {
-   if (window.innerWidth > 620 ) {
+   if (window.innerWidth > 625 ) {
       nav.style.display = "flex";
       button.classList.remove('open');
    } else {
